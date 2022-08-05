a67eeb0888b86511d70645c47b8f7b448e9d02c7
abhijnaashetty <abhijnashetty2001@gmail.com>
2022-08-05 20:24:35 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+import { QualificationComponent } from './qualification/qualification.component';
+import { SkillsComponent } from './skills/skills.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { QualitiesComponent } from './qualities/qualities.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'qualification',component:QualificationComponent},
+  {path:'skills',component:SkillsComponent},
+  {path:'achievements',component:AchievementsComponent},
+  {path:'qualities',component:QualitiesComponent},
+  {path:'projects',component:ProjectsComponent},
+  {path:'contacts',component:ContactsComponent},
+  {path:'',component:FooterComponent}
+
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { QualificationComponent } from './qualification/qualification.component';
+import { SkillsComponent } from './skills/skills.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { QualitiesComponent } from './qualities/qualities.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AboutComponent,
+    HomeComponent,
+    QualificationComponent,
+    SkillsComponent,
+    AchievementsComponent,
+    FooterComponent,
+    ContactsComponent,
+    ProjectsComponent,
+    QualitiesComponent
   ],
   imports: [
     BrowserModule,
